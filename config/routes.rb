47f8e85956da8e42e4e6fe2b66e266ed665f646f
niceonionslice <niceonionslice@gmail.com>
2019-07-27 01:45:43 +0900

    add: routesの設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  namespace :api, defaults: { format: 'json'} do
+    namespace :v1 do
+      get 'what_time_is_it', to: 'what_is_today#index'
+    end
+  end
 end
