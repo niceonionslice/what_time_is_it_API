b1c5189eeeb63bfb927a7da83df94cf56b23671c
niceonionslice <niceonionslice@gmail.com>
2019-07-29 22:52:26 +0900

    add: 日付を算出する実装を別のクラスに切り出しを実施した。

--- app/controllers/api/v1/what_is_today_controller.rb
+++ app/controllers/api/v1/what_is_today_controller.rb
@@ -6,7 +6,9 @@ module Api
         logger.debug("リクエスト #{request.url}")
         logger.debug("メソッド #{request.method}")
         logger.debug("ユーザーエージェント #{request.user_agent}")
-        @it = Date.today
+        # @it = Date.today
+        @it = WhatTimeIsIt.new(Date.today).time_now
+        logger.debug(@it)
         render 'index', formats: 'json', handlers: 'jbuilder'
       end
     end
